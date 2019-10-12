aca3773fa7c256ff998ec9583a9fee951adcfb1b
Danilo Cianfrone <danilocianfr@gmail.com>
2019-10-12 11:50:41 +0200

    Add deploy script for public submodule

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# Build the project.
+hugo
+
+# Add changes to git
+cd public
+git add .
+
+msg="Rebuild site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push changes to Github pages branch
+git push origin gh-pages
